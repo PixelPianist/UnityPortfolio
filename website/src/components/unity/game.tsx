c62bd6b128644dc380c92685862209aafbc7e734
Michael <michaeljhart760@gmail.com>
2024-12-03 13:24:28 -0800

    chore: remove node_modules from root

---
+++ website/src/components/unity/game.tsx
@@ -0,0 +1,25 @@
+﻿import React from 'react';
+import { Unity, useUnityContext } from 'react-unity-webgl'
+interface GameProps {
+    loaderUrl: string;
+    dataUrl: string;
+    frameworkUrl: string;
+    codeUrl: string;
+}
+
+const Game: React.FC<GameProps> = ({ loaderUrl, dataUrl, frameworkUrl, codeUrl }) => {
+    const unityContext = new UnityContext({
+        loaderUrl,
+        dataUrl,
+        frameworkUrl,
+        codeUrl,
+    });
+
+    return (
+        <div>
+            <Unity unityContext={unityContext} style={{ width: "960px", height: "600px" }} />
+        </div>
+    );
+}
+
+export default Game;
