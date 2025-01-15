dc6e356c93a802797bb53a3970f86293693f03e7
Michael Hart <MichaelJHart760@gmail.com>
2025-01-15 06:21:31 -0800

    feat(website): add link to repo

--- website/src/pages/index.tsx
+++ website/src/pages/index.tsx
@@ -11,6 +11,7 @@ export default function IndexPage() {
                 subtitle="Demo of automated build & deployment for Unity games!"
             />
             <p className="text-gray-100 max-w-2xl mx-auto mt-4">
+                - This is an open source project that I have built to demonstrate automated build & deployment for Unity games. Check out the project <Link href="https://github.com/PixelPianist/UnityPortfolio" className="text-blue-500 underline">here</Link> <br/>
                 - Check out the <Link className="text-blue-500 underline" href="/games">Games</Link> and <Link href="/videos" className="text-blue-500 underline">Videos</Link> pages for more content. <br/>
                 - Check out the visual pipeline below! Scroll down to see the pipeline in action.
             </p>
